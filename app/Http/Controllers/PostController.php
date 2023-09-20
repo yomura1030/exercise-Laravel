4fe2610f7514e21bce609b649bc30d246625bb56
yomura <yomura@bulletgroup.jp>
2023-09-20 18:59:24 +0900

    ペジネーションの搭載

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts=Post::all();
+        // $posts=Post::all();
+        $posts=Post::paginate(10);
         return view('post.index', compact('posts'));
     }
 
